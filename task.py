39d25b25a232bdc7d073d8966909f8e566f2f0a4
kftrv <47657709+kftrv@users.noreply.github.com>
2019-02-28 17:08:46 +0300

    update task.py

--- task.py
+++ task.py
@@ -1,26 +1,36 @@
 import requests
 import getpass
-import configparser
+import argparse
 
-
-file_w = open("info.json", "w")
-config = configparser.ConfigParser()
-config.read("config.ini")
-username = config['common']['username']
-pull_url = config['common']['pull_url']
+parser = argparse.ArgumentParser(prog="Task")
+parser.add_argument('username', help="Name of user")
+parser.add_argument('prurl', help="URL of PR")
+parser.add_argument('--state', '-s', action="store_true", help="Show state of PR")
+parser.add_argument('--title', '-t', action="store_true", help="Show title")
+parser.add_argument('--merged', '-m', action="store_true", help="Show merge state")
+parser.add_argument('--commits', '-c', action="store_true", help="Show commits count")
+parser.add_argument('--reponame', '-r', action="store_true", help="Show name of repo")
+parser.add_argument('--ownerurl', '-o', action="store_true", help="Show owner URL")
 passwd = getpass.getpass()
+args = parser.parse_args()
+username = args.username
+pull_url = args.prurl
 
 
 def get_repo_info(uname, pwd):
-    r = requests.get(pull_url, auth=(uname, pwd))
-    out = r.json()
-    file_w.write(str(out))
-    print("State: %s " % str(out['state']))
-    print("Title: %s " % str(out['title']))
-    print("Merged: %s " % str(out['merged']))
-    print("Commits: %s " % str(out['commits']))
-    print("Repo name: %s " % str(out['head']['repo']['name']))
-    print("Owner URL: %s " % str(out['base']['repo']['owner']['html_url']))
+    r = requests.get(pull_url, auth=(uname, pwd)).json()
+    if args.state:
+        print("State: %s " % r['state'])
+    if args.title:
+        print("Title: %s " % str(r['title']))
+    if args.merged:
+        print("Merged: %s " % str(r['merged']))
+    if args.commits:
+        print("Commits: %s " % str(r['commits']))
+    if args.reponame:
+        print("Repo name: %s " % r['head']['repo']['name'])
+    if args.ownerurl:
+        print("Owner URL: %s " % r['base']['repo']['owner']['html_url'])
 
 
 if __name__ == '__main__':
